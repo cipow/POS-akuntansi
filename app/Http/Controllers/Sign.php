24447222d91791fdd2cf96570dbc203495fb8e1c
cipow <cipoghost@gmail.com>
2019-01-06 14:51:34 +0700

    function login and register

---
+++ app/Http/Controllers/Sign.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Exception;
+use JWT;
+
+
+class Sign extends Controller
+{
+
+    public function __construct(){
+      parent::__construct();
+    }
+
+    public function in(Request $req) {
+      $rule = [
+        'email'     => 'required|email|max:191',
+        'password'  => 'required|string|min:6|max:32',
+      ];
+
+      if ($invalid = $this->response->validate($req, $rule)) return $invalid;
+
+      try {
+        $token = JWT::attempt($req->all());
+        return $this->response->data(['token' => $token]);
+      } catch (Exception $e) {
+        return $this->response->messageError($e->getMessage(), 400);
+      }
+
+    }
+
+    public function up(Request $req) {
+      $rule = [
+        'nama'      => 'required|string|max:191',
+        'email'     => 'required|email|unique:users|max:191',
+        'password'  => 'required|string|min:6|max:32',
+      ];
+
+      if ($invalid = $this->response->validate($req, $rule)) return $invalid;
+
+      try {
+        $user = User::create([
+          'nama'      => $req->nama,
+          'email'     => $req->email,
+          'password'  => Hash::make($req->password)
+        ]);
+
+        return $this->response->messageSuccess('berhasil daftar, silahkan login.', 201);
+      } catch (Exception $e) {
+        return $this->response->serverError();
+      }
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->post('/login', 'Sign@in');
+$router->post('/register', 'Sign@up');
