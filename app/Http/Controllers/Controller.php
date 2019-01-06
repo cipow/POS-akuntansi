921ed8bacc79f17d6793238b0e042144e7ecab1f
cipow <cipoghost@gmail.com>
2019-01-06 14:50:55 +0700

    config base controller for modified rest

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Res;
 
 class Controller extends BaseController
 {
-    //
+    protected $response;
+
+    public function __construct() {
+      $this->response = new Res();
+    }
 }
